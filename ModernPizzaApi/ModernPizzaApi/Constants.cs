16fc74dfe52b3f45853e9f9b550cf4cbaf750b7d
mateusz_wolski113 <wolski.mateusz113@gmail.com>
2020-03-08 21:10:48 +0100

    Models provided to webapi solution

---
+++ ModernPizzaApi/ModernPizzaApi/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernPizzaApi
+{
+    public static class Constants
+    {
+        public const string MONGODB_CONNECTION_STR = "mongodb://localhost";
+    }
+}
